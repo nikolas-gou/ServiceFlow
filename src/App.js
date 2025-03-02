037187e74d351a003a254c1af6df935316116db8
nikolas-gou <nikolasgou23@gmail.com>
2025-03-02 20:03:49 +0200

    Adding Layout component, css for sidebar, and routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { ThemeProvider, CssBaseline } from "@mui/material";
+import { ThemeProvider, CssBaseline, Typography } from "@mui/material";
 
 import theme from "./styles/Themes";
 import SideBar from "./components/layout/SideBar";
 import TopAppBar from "./components/layout/TopAppBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/dashboard/Dashboard";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline /> {/* Resets styles for consistency */}
-      <SideBar />
+      <BrowserRouter>
+        <CssBaseline /> {/* Resets styles for consistency */}
+        <Layout sideBar={<SideBar />}>
+          <SideBar />
+          <Routes>
+            <Route
+              exact
+              path="/dashboard/overview"
+              element={<Typography>overview</Typography>}
+            />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
---
+++ src/components/layout/Layout.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Box } from "@mui/material";
+import SideBar from "./SideBar";
+import TopAppBar from "./TopAppBar";
+
+const drawerWidth = 250;
+
+const Layout = (props) => {
+  return (
+    <Box sx={{ display: "flex", height: "100vh" }}>
+      {/* Sidebar */}
+      <Box sx={{ width: drawerWidth, minWidth: drawerWidth, flexShrink: 0 }}>
+        <SideBar />
+      </Box>
+
+      {/* Κεντρικό περιεχόμενο (Αφήνει χώρο για το Sidebar) */}
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          overflow: "hidden",
+        }}
+      >
+        {/* Scrollable Content */}
+        <Box sx={{ flexGrow: 1, p: 3, overflowY: "auto" }}>
+          {props.children}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
