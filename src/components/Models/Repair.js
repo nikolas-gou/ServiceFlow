d043d7b6fd3718582d7f3f41535842546d8329bf
nikolas-gou <nikolasgou23@gmail.com>
2025-03-17 12:25:15 +0200

    Adding new fields, methos toAPI and modifying toJson

--- src/components/Models/Repair.js
+++ src/components/Models/Repair.js
@@ -4,31 +4,45 @@ import { Motor } from "./Motor";
 export class Repair {
   constructor(data = {}) {
     this.id = data.id || null;
-    this.motorID = data.motorID || "";
-    this.customerID = data.customerID || "";
-    this.repair_status = data.repair_status || "";
+    this.motorID = data.motorID || null;
+    this.customerID = data.customerID || null;
+    this.repair_status = data.repair_status || "In-progress";
     this.description = data.description || "";
     this.cost = data.cost || "";
-    this.created_at = data.created_at || "";
-    this.customer = data.customer ? new Customer(data.customer) : null;
-    this.motor = data.motor ? new Motor(data.motor) : null;
+    this.created_at = data.created_at || new Date().toISOString().split("T")[0];
+    this.estimatedIsComplete =
+      data.estimatedIsComplete || new Date().toISOString().split("T")[0];
+    this.customer = data.customer
+      ? new Customer(data.customer)
+      : new Customer();
+    this.motor = data.motor ? new Motor(data.motor) : new Motor();
   }
-
-  //   isValid() {
-  //     return this.name.trim() !== "" && this.phone.trim() !== "";
-  //   }
 
   toJSON() {
     return {
       id: this.id,
-      motorID: this.this.motorID,
-      customerID: this.this.customerID,
+      motorID: this.motorID,
+      customerID: this.customerID,
       repair_status: this.repair_status,
       created_at: this.created_at,
+      estimatedIsComplete: this.estimatedIsComplete,
       description: this.description,
       cost: this.cost,
       customer: this.customer ? this.customer.toJSON() : null,
       motor: this.motor ? this.motor.toJSON() : null,
+    };
+  }
+
+  toApiFormat() {
+    return {
+      id: this.id,
+      motorID: this.motor.id,
+      customerID: this.customer.id,
+      description: this.description,
+      repair_status: this.repair_status,
+      created_at: this.created_at,
+      estimatedIsComplete: this.estimatedIsComplete,
+      cost: this.cost,
     };
   }
 }
