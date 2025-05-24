23df88327230a4038a4a5a813a722c32d1bfba4d
nikolas-gou <nikolasgou23@gmail.com>
2025-05-24 11:26:18 +0300

    Adding new fields

--- src/components/Models/Motor.js
+++ src/components/Models/Motor.js
@@ -7,11 +7,23 @@ export class Motor {
     this.hp = data.hp || "";
     this.rpm = data.rpm || "";
     this.step = data.step || "";
+    this.halfStep = data.halfStep || "";
+    this.helperStep = data.helperStep || "";
+    this.helperHalfStep = data.helperHalfStep || "";
     this.spiral = data.spiral || "";
+    this.halfSpiral = data.halfSpiral || "";
+    this.helperSpiral = data.helperSpiral || "";
+    this.helperHalfSpiral = data.helperHalfSpiral || "";
     this.cross_section = data.cross_section || "";
+    this.halfCross_section = data.halfCross_section || "";
+    this.helperCross_section = data.helperCross_section || "";
+    this.helperHalfCross_section = data.helperHalfCross_section || "";
     this.connectionism = data.connectionism || "";
     this.volt = data.volt || "";
     this.poles = data.poles || "";
+    this.typeOfStep = data.typeOfStep || "standard";
+    this.typeOfMotor = data.typeOfMotor || "el_motor";
+    this.typeOfVolt = data.typeOfVolt || "3-phase";
     this.created_at = data.created_at || new Date().toISOString().split("T")[0];
     this.customerID = data.customerID || "";
   }
@@ -29,11 +41,23 @@ export class Motor {
       hp: this.hp,
       rpm: this.rpm,
       step: this.step,
+      halfStep: this.halfStep,
+      helperStep: this.helperStep,
+      helperHalfStep: this.helperHalfStep,
       spiral: this.spiral,
+      halfSpiral: this.halfSpiral,
+      helperSpiral: this.helperSpiral,
+      helperHalfSpiral: this.helperHalfSpiral,
       cross_section: this.cross_section,
+      halfCross_section: this.halfCross_section,
+      helperCross_section: this.helperCross_section,
+      helperHalfCross_section: this.helperHalfCross_section,
       connectionism: this.connectionism,
       volt: this.volt,
       poles: this.poles,
+      typeOfStep: this.typeOfStep,
+      typeOfMotor: this.typeOfMotor,
+      typeOfVolt: this.typeOfVolt,
       created_at: this.created_at,
       customerID: this.customerID,
     };
@@ -79,3 +103,16 @@ export const volt_types_mapping = {
   "380VYY": "380VΔ",
   other: "Αλλο",
 };
+
+export const typeOfVolt = ["1-phase", "3-phase"];
+export const typeOfVolt_translated = ["Μονοφασικός", "Τριφασικός"];
+
+export const typeOfMotor = ["el_motor", "pump", "generator"];
+export const typeOfMotor_translated = ["Μοτέρ", "Αντλία", "Γεννήτρια"];
+
+export const typeOfStep = ["standard", "half", "combined"];
+export const typeOfStep_translated = [
+  "Ολόκληρο",
+  "Μισό-Μισό",
+  "Μισό-Μισό και Ολόκληρο",
+];
