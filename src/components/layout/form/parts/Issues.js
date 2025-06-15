4c80978a29914150efe45a4defc60a9224779a5f
nikolas-gou <nikolasgou23@gmail.com>
2025-06-15 15:34:29 +0300

    Creating new component for common issues

---
+++ src/components/layout/form/parts/Issues.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { Grid, TextField, Typography, Box, Button } from '@mui/material';
+import { common_faults, common_faults_translated } from '../../../Models/CommonFault';
+
+export const Issues = (props) => {
+  const [selectedFault, setSelectedFault] = useState([]);
+
+  // Προσθήκη συνηθισμένων θεμάτων στην περιγραφή
+  const handleAddCommonIssue = (common_fault) => {
+    const updatedDescription = props.repair.description
+      ? `${props.repair.description}\n- ${common_faults_translated[common_fault.id - 1].name}`
+      : `- ${common_faults_translated[common_fault.id - 1].name}`;
+
+    // Δημιουργία νέας λίστας repairFaultLinks με το νέο link αν δεν υπάρχει ήδη
+    const newFaultLink = { repairID: props.repair.id, commonFaultID: common_fault.id };
+
+    // Αποφυγή διπλοεγγραφών
+    const alreadyExists = props.repair.repairFaultLinks.some(
+      (link) => link.commonFaultID === common_fault.id,
+    );
+
+    props.setRepair((prev) => ({
+      ...prev,
+      description: updatedDescription,
+      repairFaultLinks: alreadyExists
+        ? prev.repairFaultLinks
+        : [...prev.repairFaultLinks, newFaultLink],
+    }));
+
+    // Προσθήκη του ID της βλάβης στον πίνακα, αν δεν υπάρχει ήδη
+    setSelectedFault((prev) => {
+      if (!prev.includes(common_fault.id)) {
+        return [...prev, common_fault.id];
+      }
+      return prev;
+    });
+
+    // Καθαρισμός τυχόν σφαλμάτων
+    if (props.errors.description) {
+      props.setErrors((prev) => ({
+        ...prev,
+        description: null,
+      }));
+    }
+  };
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Typography variant="subtitle2" gutterBottom>
+          Συχνές βλάβες:
+        </Typography>
+        <Box sx={{ mb: 2 }}>
+          {common_faults.map((common_fault) => (
+            <Button
+              key={common_fault.id}
+              variant="outlined"
+              size="small"
+              sx={{ mr: 1, mb: 1 }}
+              onClick={() => handleAddCommonIssue(common_fault)}
+            >
+              {common_faults_translated[common_fault?.id - 1]?.name}
+            </Button>
+          ))}
+        </Box>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          required
+          label="Περιγραφή Βλάβης"
+          name="description"
+          multiline
+          rows={6}
+          variant="outlined"
+          value={props.repair.description || ''}
+          onChange={props.handleInputChange}
+          error={props.hasError('description')}
+          helperText={props.getErrorMessage('description')}
+        />
+      </Grid>
+    </Grid>
+  );
+};
