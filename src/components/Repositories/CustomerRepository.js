985635616d272de12369f5b7aac07adbf1a76f4d
nikolas-gou <nikolasgou23@gmail.com>
2025-07-12 21:26:34 +0300

    Removing useless functions on statistic repo

--- src/components/Repositories/CustomerRepository.js
+++ src/components/Repositories/CustomerRepository.js
@@ -1,6 +1,5 @@
 // repositories/CustomerRepository.js
 import config from '../../config';
-import { Customer } from '../Models/Customer';
 import apiCall from '../../utils/apiCall';
 
 export class CustomerRepository {
@@ -9,11 +8,6 @@ export class CustomerRepository {
     // Το backend επιστρέφει ήδη formatted data
     const data = response.data || [];
     return data;
-  }
-
-  static async getStats() {
-    const response = await apiCall(config.server, '/api/statsOfCustomer', 'GET');
-    return response.data || {};
   }
 
   static async createNewCustomer(customer) {
@@ -26,53 +20,4 @@ export class CustomerRepository {
     const data = response.data || [];
     return data.map((customer) => customer.name);
   }
-
-  static async getStatisticsOverview() {
-    const response = await apiCall(config.server, '/api/statistics/overview', 'GET');
-    return response.data || {};
-  }
-
-  //   static async getById(id) {
-  //     const response = await apiCall(config.server, `/customers/${id}`, "GET");
-  //     const data = response.data || {};
-  //     return new Customer(data);
-  //   }
-
-  //   static async create(customer) {
-  //     const response = await apiCall(
-  //       config.server,
-  //       "/customers",
-  //       "POST",
-  //       customer.toJSON()
-  //     );
-  //     const data = response.data || {};
-  //     return new Customer(data);
-  //   }
-
-  //   static async update(customer) {
-  //     const response = await apiCall(
-  //       config.server,
-  //       `/customers/${customer.id}`,
-  //       "PUT",
-  //       customer.toJSON()
-  //     );
-  //     const data = response.data || {};
-  //     return new Customer(data);
-  //   }
-
-  //   static async delete(id) {
-  //     await apiCall(config.server, `/customers/${id}`, "DELETE");
-  //     return true;
-  //   }
-
-  //   // Επιπλέον μέθοδοι
-  //   static async search(query) {
-  //     const response = await apiCall(
-  //       config.server,
-  //       `/customers/search?q=${encodeURIComponent(query)}`,
-  //       "GET"
-  //     );
-  //     const data = response.data || [];
-  //     return data.map((customer) => new Customer(customer));
-  //   }
 }
