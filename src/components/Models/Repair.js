3f7f376d97266b7dd5fbcd5667942bbec6192b1a
nikolas-gou <nikolasgou23@gmail.com>
2025-10-08 23:59:24 +0300

    Creating new function getRepairById on repairRepository,js

--- src/components/Models/Repair.js
+++ src/components/Models/Repair.js
@@ -1,6 +1,7 @@
 import { Customer } from './Customer';
 import { Motor } from './Motor';
 import { RepairFaultLinks } from './RepairFaultLinks';
+import { Image } from './Image';
 
 export class Repair {
   constructor(data = {}) {
@@ -13,10 +14,14 @@ export class Repair {
     this.createdAt = data.createdAt || new Date();
     this.isArrived = data.isArrived || new Date().toISOString().split('T')[0];
     this.estimatedIsComplete = data.estimatedIsComplete || new Date().toISOString().split('T')[0];
+    this.deletedAt = data.deletedAt || null;
     this.customer = data.customer ? new Customer(data.customer) : new Customer();
     this.motor = data.motor ? new Motor(data.motor) : new Motor();
     this.repairFaultLinks = Array.isArray(data.repairFaultLinks)
       ? data.repairFaultLinks.map((link) => new RepairFaultLinks(link))
+      : [];
+    this.images = Array.isArray(data.images)
+      ? data.images.map((image) => (image instanceof Image ? image : new Image(image)))
       : [];
   }
 
@@ -31,11 +36,13 @@ export class Repair {
       estimatedIsComplete: this.estimatedIsComplete,
       description: this.description,
       cost: this.cost,
+      deletedAt: this.deletedAt,
       customer: this.customer ? this.customer.toJSON() : null,
       motor: this.motor ? this.motor.toJSON() : null,
       repairFaultLinks: this.repairFaultLinks
         ? this.repairFaultLinks.map((link) => link.toJSON())
         : [],
+      images: this.images ? this.images.map((image) => image.toJSON()) : [],
     };
   }
 }
