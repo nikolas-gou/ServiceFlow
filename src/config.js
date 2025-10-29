0881f0b88e55a8269bf61d9ad09cb1646e079153
nikolas-gou <nikolasgou23@gmail.com>
2025-10-29 21:10:19 +0200

    Update config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const config = {
   // server: 'http://192.168.148.230:8000', hotspot mobile
-  // server: 'http://192.168.2.8:8000', home wifi
-  server: 'http://localhost:8000',
+  server: 'http://192.168.2.8:8000', //home wifi
+  // server: 'http://localhost:8000', //localhost
   dateFormat: 'el-GR',
 };
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'development') {
-  config.server = 'https://serviceflowback.infinityfreeapp.com';
+  config.server = 'http://192.168.2.8:8000'; //home wifi
 }
 
 export default config;
